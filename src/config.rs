a7c0d7962f31f373c187d78d0fb6414a016eb41f
brkfce <calsutton12@gmail.com>
2022-05-11 23:11:38 +0100

    Config file read in.

--- src/config.rs
+++ src/config.rs
@@ -1,1 +1,28 @@
-use serde;
+use serde::Deserialize;
+use std::fs::File;
+use std::io::ErrorKind;
+
+/*
+This mod opens a config.json file, which contains the newsapi key and user preferences.
+At the moment, there are no user preferences, only the API key.
+*/
+
+#[derive(Debug, Deserialize)]
+#[serde(rename_all = "PascalCase")]
+struct config {
+	api_key: String
+}
+
+
+pub fn load_config() -> config {
+
+	// read config file
+	let file = File::open("config.json");
+	let file = match file {
+		Ok(file) => file,
+		Err(error) => match error.kind() {
+			ErrorKind::NotFound => panic!("Config file could not be found."),
+			other_error => panic!("Error trying to open config file.")
+		}
+	};
+}
