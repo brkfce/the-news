35382d9b272f42c14505cdb65acf434d2eb4b90b
brkfce <calsutton12@gmail.com>
2022-05-12 13:16:31 +0100

    Updated lib to get response from API.

--- src/lib.rs
+++ src/lib.rs
@@ -4,4 +4,5 @@ mod config;
 
 pub fn get_the_news() {
 	let user_config: config::Config = config::load_config();
+	let response: fetch_data::response_object = fetch_data::fetch_data(user_config);
 }
