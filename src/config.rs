fbd075d4777a44fa5032d6c8e48bcca5d2364df4
brkfce <calsutton12@gmail.com>
2022-05-12 10:46:29 +0100

    Added comments.

--- src/config.rs
+++ src/config.rs
@@ -10,10 +10,11 @@ At the moment, there are no user preferences, only the API key.
 #[derive(Debug, Deserialize)]
 #[serde(rename_all = "PascalCase")]
 pub struct Config {
+	// the api key needed for the newsapi
 	api_key: String
 }
 
-
+// entry point to module
 pub fn load_config() -> Config {
 
 	let config_filepath = "config.json";
@@ -26,6 +27,7 @@ pub fn load_config() -> Config {
 	
 }
 
+// reads the contents of the config file
 fn open_file(filepath: &'static str) -> String {
 
 	// read config file
@@ -42,6 +44,7 @@ fn open_file(filepath: &'static str) -> String {
 	file_contents
 }
 
+// the config file is in the JSON format, so it needs to be deserialized and stored in a config struct
 fn parse_json(file_contents: String) -> Config {
 
 	// deserialise config file into config struct
@@ -56,7 +59,7 @@ fn parse_json(file_contents: String) -> Config {
 #[cfg(test)]
 mod tests {
 	
-
+	// test a file string that is formatted correctly
 	#[test]
 	fn test_correct_deserialise() {
 		let correct_json_string: String = "{ \"ApiKey\":\"123\"}".to_string();
@@ -64,6 +67,7 @@ mod tests {
 		assert_eq!("123", configuration.api_key);
 	}
 
+	// test a file string that is not in a JSON format
 	#[test]
 	#[should_panic]
 	fn test_incorrect_json() {
@@ -71,6 +75,7 @@ mod tests {
 		let _configuration: super::Config = super::parse_json(incorrect_json_string);
 	}
 
+	// test a JSON file string that does not contain the correct parameters
 	#[test]
 	#[should_panic]
 	fn test_incorrect_config() {
