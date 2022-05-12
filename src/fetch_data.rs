281c239a3f6d8d5546ae536963048cbd775c4fd9
brkfce <calsutton12@gmail.com>
2022-05-12 11:22:54 +0100

    Added objects to store the newsapi response.

--- src/fetch_data.rs
+++ src/fetch_data.rs
@@ -1,2 +1,40 @@
 use reqwest;
 use tokio;
+use serde::Deserialize;
+use crate::config;
+
+
+// the object returned by the newsapi
+#[derive(Debug, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct response_object {
+	status: String,
+	total_results: i32,
+	articles: Vec<article_object>
+}
+#[derive(Debug, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct article_object {
+	source: article_source_object,
+	author: String,
+	title: String,
+	description: String,
+	url: String,
+	url_to_image: String,
+	published_at: String,
+	content: String
+}
+
+#[derive(Debug, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct article_source_object {
+	id: String,
+	name: String
+}
+
+
+// entry point to module
+pub fn get_data(configuration: config::Config) {
+
+}
+
