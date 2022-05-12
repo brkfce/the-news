7f66b133c826f2390d8299555660672b31e5b914
brkfce <calsutton12@gmail.com>
2022-05-12 22:38:02 +0100

    Top headlines are printed to standard output.

--- src/display.rs
+++ src/display.rs
@@ -4,6 +4,12 @@ use crate::fetch_data;
 // display the news headlines to the user
 pub fn headlines(response: fetch_data::ResponseObject) {
 
-	// test value; print the number of results
-	println!("The number of results is: {}", response.total_results);
+	let mut counter = 1;
+	
+	for headline in response.articles {
+
+		// print title of each article fetched
+		println!("{}. {}", counter, headline.title);
+		counter += 1;
+	}
 }
