2e630440ff6606170f08b1aea21a024c7203b8c9
brkfce <calsutton12@gmail.com>
2022-05-12 13:30:04 +0100

    Included display in lib file.

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,10 @@
 mod fetch_data;
 mod config;
+mod display;
 
 
 pub fn get_the_news() {
 	let user_config: config::Config = config::load_config();
 	let response: fetch_data::response_object = fetch_data::fetch_data(user_config);
+	display::headlines(response);
 }
